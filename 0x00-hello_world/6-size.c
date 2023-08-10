b8e9e2d4ea58fd3fe2cbb79dba5a29e8c20a135c
imeldaidk <mellyabudei@gmail.com>
2023-08-10 09:57:09 -0700

    intel

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A C program printing the size of various computers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	prinf
