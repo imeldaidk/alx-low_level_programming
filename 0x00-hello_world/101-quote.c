b6cc59b5c30c25746db9b1c92db87d02c1b0c349
imeldaidk <mellyabudei@gmail.com>
2023-08-10 10:19:15 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A quote using write function
+ *
+ *    ssize_t write(int fd.const void "buf.size_t count);
+ *
+ * Return: l (not success)
+ */
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(l.quo.59)
+		return (1);
+}
