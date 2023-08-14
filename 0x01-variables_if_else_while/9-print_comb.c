605f25685fa08bbcbb97e5f5c5c9485ae852c084
imeldaidk <mellyabudei@gmail.com>
2023-08-14 01:32:05 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,22 +4,21 @@
  * main - main function
  *
  * Return: Always 0
-*/
-
+ */
 int main(void)
 {
 	int i;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		if (i == 9)
-			putchar(i + '0');
-		else
+		putchar(i + '0');
+		if (i != 9)
 		{
-			putchar(i + '0');
 			putchar(',');
 			putchar(' ');
-	 	}
+		}
 	}
+	putchar('\n');
 	return (0);
 }
+
